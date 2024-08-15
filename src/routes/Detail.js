f95d10abe083c1546265d23d0232264f14726982
데스크탑 <z_gahu@naver.com>
2024-08-15 11:43:08 +0900

    상세페이지에서 상품을 불러올때 순서대 id 를 사용하도록 변경

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,22 +2,30 @@ import { useParams } from "react-router-dom";
 
 function Detail(props) {
   let { id } = useParams();
-  console.log("id", id);
+  const pngNum = Number(id) + 1;
+  console.log("id", id, pngNum);
 
+  // let 찾은상품 = props.shoes.find(function (x) {
+  //   return x.id == id;
+  // });
+
+  let 찾은상품 = props.shoes.find((x) => x.id.toString() === id);
+
+  console.log("찾은상품", 찾은상품);
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6">
           <img
-            src={"https://codingapple1.github.io/shop/shoes" + id + ".jpg"}
+            src={"https://codingapple1.github.io/shop/shoes" + pngNum + ".jpg"}
             width="100%"
             alt="shoes"
           />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">{props.shoes[id].title}</h4>
-          <p>{props.shoes[id].content}</p>
-          <p>{props.shoes[id].price}</p>
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
